018902ccebb6973c1443a93e3d98bf98175d36c2
Maksim <maksim@maksim-ubuntu.(none)>
2014-10-04 05:47:41 +0400

    improving code

--- HtmlHelper.php
+++ HtmlHelper.php
@@ -1,10 +1,8 @@
 <?php
    class HtmlHelper {
       private $paginginfo;
-      public function set_paginginfo($paginginfo) {
+      public function show_page_links($paginginfo, $scriptname) {
          $this->paginginfo = $paginginfo;
-      }
-      public function show_page_links($scriptname) {
          $n = $this->paginginfo->total_pages;
          $curr_page = $this->paginginfo->current_page; 
          for($i = 1; $i <= $n; $i++) {
--- List.php
+++ List.php
@@ -3,14 +3,13 @@
 <head>
    <meta charset="utf-8" />
    <title>List of products</title>
-   <link rel="stylesheet" type="text/css" href="sitestyle.css" />
 </head>
 <body>
    <?php
       require_once("PagingInfo.php");
       require_once("ProductRepository.php");
       require_once("HtmlHelper.php");
-      define('THIS_SCRIPT_NAME', 'productList.php');
+      define('THIS_SCRIPT_NAME', 'List.php');
       
       // Задаём параметры разбиения на страницы:
       $pagingInfo = new PagingInfo();
@@ -20,19 +19,14 @@
 
       // Извлечение и показ списка товаров:
       $productRepository = new ProductRepository();
-      $productRepository->extract_data_from_db( 
-         // начальная позиция извлечения данных из базы
-         ($pagingInfo->current_page - 1)*$pagingInfo->items_per_page, 
-         $pagingInfo->items_per_page // количество извлекаемых строк
-      );
+      $start = ($pagingInfo->current_page - 1)*$pagingInfo->items_per_page; 
+      $productRepository->extract_data_from_db($start, $pagingInfo->items_per_page); 
       $productRepository->show_extracted_data_on_this_page(); 
    ?>
    <p>
-   <?php
-      // Отображение ссылок на страницы:
+   <?php // Отображение ссылок на страницы:
       $htmlhelper = new HtmlHelper();
-      $htmlhelper->set_paginginfo($pagingInfo);
-      $htmlhelper->show_page_links(THIS_SCRIPT_NAME);
+      $htmlhelper->show_page_links($pagingInfo, THIS_SCRIPT_NAME);
    ?>
    </p>
 </body>
